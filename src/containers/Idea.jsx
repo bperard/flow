6e616e81228777ad38217dc1712ab5a526e0d89b
Brandon Perard <brandon.perard@gmail>
2021-07-18 18:02:15 -0700

    add initial element structure of div containing input(s) and connect input onChange values to state

--- src/containers/Idea.jsx
+++ src/containers/Idea.jsx
@@ -9,7 +9,62 @@ const Idea = () => {
     const [steps, setSteps] = useState([]);
 
     return (
-        <h1>This may be the start of something...</h1>
+        <div id="idea-div">
+            <div id="title-div">
+                <input
+                    type="text"
+                    placeholder="Enter project name"
+                    onChange={(e) => {
+                        setTitle({
+                            ...title,
+                            name: e.target.value,
+                        });
+                    }} />
+                <input
+                    type="text"
+                    placeholder="Enter project description"
+                    onChange={(e) => {
+                        setTitle({
+                            ...title,
+                            description: e.target.value,
+                        });
+                    }} />
+            </div>
+            <div id="filters-div">
+                <input
+                    type="text"
+                    placeholder="Enter new filter"
+                    onChange={(e) => {  // add map index value for dynamic value
+                        const currentFilters = filters;
+                        currentFilters[0].name = e.target.value;
+                        setFilters([
+                            ...currentFilters,
+                        ]);
+                    }} />
+                <input
+                    type="text"
+                    placeholder="Enter filter abbreviation"
+                    onChange={(e) => {  // add map index value for dynamic value
+                        const currentFilters = filters;
+                        currentFilters[0].abbreviation = e.target.value;
+                        setFilters([
+                            ...currentFilters,
+                        ]);
+                    }} />
+            </div>
+            <div id="steps-div">
+                <input
+                    type="text"
+                    placeholder="Enter new step"
+                    onChange={(e) => {  // add map index value for dynamic value
+                        const currentSteps = steps;
+                        currentSteps[0] = e.target.value;
+                        setSteps([
+                            ...currentSteps,
+                        ]);
+                    }} />
+            </div>
+        </div>
     );
 };
 
